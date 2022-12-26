a33f2406f7802c08bf60a54615654bfdac3fb764
Ali Şen <alisen4099@gmail.com>
2022-12-26 17:58:56 +0300

    updated

--- src/main/java/com/springboot/whetherapp/WhetherAppApplication.java
+++ src/main/java/com/springboot/whetherapp/WhetherAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WhetherAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WhetherAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WhetherAppApplication.class, args);
+    }
 
 }
